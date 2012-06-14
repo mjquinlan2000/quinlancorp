d81c0aa757f64071746edc83634228add7e2802d
Michael Quinlan <mjquinlan2000@gmail.com>
2012-06-13 18:13:46 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quinlancorp::Application.config.secret_token = '85f3a18583cfd4bae64d5a93b3444ebb585b2634dbfbefd00f75a19b7e1d21c575142ed92806d957a5dd58463089e991fad4e69d6c275473d0585dff0b6e128e'
